868c69dd2a1379cf185083497f49f48106be78e5
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-28 20:37:18 -0600

    Created login form component and began import of common components (button)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyBtz3PMIf9M2g4umrn8607VI5mQHZSsF2c',
+    authDomain: 'auth-f62f5.firebaseapp.com',
+    databaseURL: 'https://auth-f62f5.firebaseio.com',
+    projectId: 'auth-f62f5',
+    storageBucket: 'auth-f62f5.appspot.com',
+    messagingSenderId: '585886201877'
+  });
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
     )
   }
