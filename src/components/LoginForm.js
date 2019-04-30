1dfbf4894c9addff089aef34187ae1cb1edc61f9
Daniel Pak <Pakjuhnman@gmail.com>
2019-04-29 20:09:26 -0600

    refactor of loginform.js and added placeholder and autocorrect props

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,15 +4,17 @@ import { Button, Card, CardSection, Input } from './common';
 
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            label="Email"
+            placeholder="example@email.com"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
         <CardSection />
