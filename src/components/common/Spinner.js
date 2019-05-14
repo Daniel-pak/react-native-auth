60a33fbab7783c8820c8acfc005d6ea5efbdb071
Daniel Pak <Pakjuhnman@gmail.com>
2019-05-14 07:54:37 -0600

    Adding in spinner reusable component

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+const Spinner = () => {
+  return (
+    <View />
+  );
+}
+
+export { Spinner }; //Because this is a reusable component, we use Spinner in braces.
