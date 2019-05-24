c85ba8ad1d50b17d89e864463f727660eed63bb3
Daniel Pak <Pakjuhnman@gmail.com>
2019-05-24 11:58:12 -0600

    Adding onButton press function to log user out. Application has been tested and a user can log in and out using the buttons and input fields.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  state = { loggedIn: false };
+  state = { loggedIn: null };
 
   componentWillMount() {
     firebase.initializeApp({
@@ -17,20 +17,35 @@ class App extends Component {
     messagingSenderId: '585886201877'
     });
 
-    firebase.auth().onAuthStateChange((user) => {
-      if (true) {
-        this.setState({ loggedIn: true }); 
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
       } else {
         this.setState({ loggedIn: false });
       }
     });
   }
 
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
